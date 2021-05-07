05678861939f6edbb60bc497f31c8b75aa54c0bc
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-05-07 19:04:52 +0300

    Migrate to tonephp v0.1.8

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,3 @@ use core\Router;
 
 // custom route here
 // ...
-
-// default routes
-Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
-Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
